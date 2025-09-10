d7dcac41aa42d13b1188c36932288fb98ec8fe02
Ping Zhang <zhangping@infrawaves.com>
2025-09-10 20:42:10 +0800

    Fix comments: deduplicate code

--- siirl/utils/memory_utils.py
+++ siirl/utils/memory_utils.py
@@ -15,40 +15,11 @@
 
 import gc
 import logging
-import torch
 
-# from verl.utils.device import get_torch_device
+from siirl.utils.extras.device import get_torch_device
 
 logger = logging.getLogger(__name__)
 
-is_cuda_available = torch.cuda.is_available()
-# is_npu_available = is_torch_npu_available()
-
-def get_device_name() -> str:
-    """Function that gets the torch.device based on the current machine.
-    This currently only supports CPU, CUDA, NPU.
-    Returns:
-        device
-    """
-    if is_cuda_available:
-        device = "cuda"
-    # elif is_npu_available:
-    #     device = "npu"
-    else:
-        device = "cpu"
-    return device
-
-def get_torch_device() -> any:
-    """Return the corresponding torch attribute based on the device type string.
-    Returns:
-        module: The corresponding torch device namespace, or torch.cuda if not found.
-    """
-    device_name = get_device_name()
-    try:
-        return getattr(torch, device_name)
-    except AttributeError:
-        logger.warning(f"Device namespace '{device_name}' not found in torch, try to load torch.cuda.")
-        return torch.cuda
 
 def aggressive_empty_cache(force_sync: bool = True, max_retries: int = 3) -> None:
     """
